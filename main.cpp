28c888ad7ff2e548fc628bd1496af77703a08968
Compotation <46853874+Compotation@users.noreply.github.com>
2023-05-22 00:54:39 -0400

    done main

--- main.cpp
+++ main.cpp
@@ -3,6 +3,13 @@
 
 int main() {
   std::vector<int> d1 = {10, 3, 5, -1};
+  std::cout << "unsorted:\n";
+  for (int val: d1) {
+    std::cout << val << ", ";
+  }
+  std::cout << "\n";
+  std::cout << "sorted:\n";
+
   heapsort(d1);
   for (int val: d1) {
     std::cout << val << ", ";
